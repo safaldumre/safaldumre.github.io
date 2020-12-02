d4a10ad75684b4a6dc9f1950c9b0ac2f3bc0acf8
safaldumre <safal_dumre@yahoo.com>
2020-12-01 20:12:24 -0600

    first commit portfolio

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Hi <?php echo $_POST["firstname"]; ?> <?php echo $_POST["lastname"]; ?>,<br>
+Thanks for reaching out! I will get back to you at <?php echo $_POST["email"]; ?> very shortly;<br>
+if the information you see above is not correct, please resubmit the form on my website <a href="AboutMe.html"> here</a>.
+If everything looks right, you can return to my webpage <a href="SafalDumre.html"> here</a>.
+
+</body>
+</html>
